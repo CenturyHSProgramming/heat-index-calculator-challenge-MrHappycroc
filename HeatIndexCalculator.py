c43d2298dbc68fe742c3b6f2b89758d9af9a3860
Wilson_Matteus <wilsm111@hsd.k12.or.us>
2017-04-25 09:36:41 -0700

    Made the thing, doesn't pass all tests. I think some tests may be wrong

--- HeatIndexCalculator.py
+++ HeatIndexCalculator.py
@@ -7,8 +7,29 @@
 # Define Function below
 # be sure to return an integer
 
+def calculateHeatIndex(humidity, tempature):
+    h = humidity
+    t = tempature
+    c1 = -42.379
+    c2 = 2.04901523
+    c3 = 10.14333127
+    c4 = -0.22475541
+    c5 = -0.00683783
+    c6 = -0.05481717
+    c7 = 0.00122874
+    c8 = 0.00085282
+    c9 = -0.00000199
+    index = c1 + (c2 * t) + (c3 * h) + (c4 * t * h) + (c5 * (t**2)) + (c6 * (h**2)) + (c7 * (t**2)*h)
+    index = index + (c8 * t * (h**2))+(c9 * (t**2) * (h**2))
+
+
+    return round(index)
+
 
 if __name__ == '__main__':
     # Call the function in here if you want to test it
     # Make sure it's indented
     pass # remove or comment out this line if you wish to test the function
+
+
+
